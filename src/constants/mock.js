885c663cf799f5234d7003174b4b50797f963faa
Andrey <Andreyadad@gmail.com>
2024-07-14 19:27:09 +0500

    Создал файл с моковыми данными

---
+++ src/constants/mock.js
@@ -0,0 +1,128 @@
+export const cinemas = [
+    {
+        id: "a757a0e0",
+        name: "Шесть звезд",
+        films: [
+            {
+                id: "d75f762a",
+                name: "ОНО",
+                price: 12,
+                actors: ["Джейден Мартелл", "София Лиллис"],
+            },
+            {
+                id: "c2cb8f92",
+                name: "ОНО2",
+                price: 14,
+                actors: ["Джессика Честейн", "Джеймс МакЭвой"],
+            },
+            {
+                id: "bd129641",
+                name: "Лето 84",
+                price: 8,
+                actors: ["Грэм Верчир", "Джуда Лькис"],
+            },
+        ],
+        reviews: [
+            {
+                id: "5909796d",
+                user: {
+                    id: "a304959a",
+                    name: "Antony",
+                },
+                text: "Not bad",
+                rating: 5,
+            },
+            {
+                id: "429dea85",
+                user: {
+                    id: "20bed9b5",
+                    name: "Diana",
+                },
+                text: "Грязно",
+                rating: 3,
+            },
+        ],
+    },
+    {
+        id: "bb8afbec",
+        name: "Синима Сад",
+        films: [
+            {
+                id: "25402233",
+                name: "Чужие",
+                price: 9,
+                actors: ["Сигурни Уивер", "Майкл Бин"],
+            },
+            {
+                id: "90902233",
+                name: "Заклятие",
+                price: 10,
+                actors: ["Вера Фармига", "Патрик Уилсон"],
+            },
+        ],
+        reviews: [
+            {
+                id: "53b642d7",
+                user: {
+                    id: "c3d4abd4",
+                    name: "Lolly",
+                },
+                text: "Хороший попкорн",
+                rating: 5,
+            },
+            {
+                id: "c27ab88e",
+                user: {
+                    id: "52a63cc0",
+                    name: "Agata",
+                },
+                text: "Жесткие стулья",
+                rating: 4,
+            },
+            {
+                id: "abc0c5e1",
+                user: {
+                    id: "1547335a",
+                    name: "Rebeca",
+                },
+                text: "Отличный звук",
+                rating: 5,
+            },
+        ],
+    },
+    {
+        id: "982bfbce",
+        name: "Прибалтика",
+        films: [
+            {
+                id: "08c9ffa0",
+                name: "Сайлент Хилл",
+                price: 5,
+                actors: ["Рада Митчел", "Джоделль Ферланд"],
+            },
+            {
+                id: "64a4967c",
+                name: "Заклятие 2",
+                price: 2,
+                actors: ["Патрик Уилсон", "Вера Фармига"],
+            },
+            {
+                id: "4bc8528e",
+                name: "Кошмар на улице Вязов",
+                price: 4,
+                actors: ["Хэзер Лэнгенкэмп", "Роберт Инглунд"],
+            },
+        ],
+        reviews: [
+            {
+                id: "12b642d7",
+                user: {
+                    id: "dfb982e9",
+                    name: "Sam",
+                },
+                text: "Удобное расположение",
+                rating: 5,
+            },
+        ],
+    },
+];
