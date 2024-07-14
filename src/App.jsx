9ddb2c488fdfe23310ca0d92add7bf31544c226d
Andrey <Andreyadad@gmail.com>
2024-07-15 00:31:55 +0500

    Рендерим то же самое только теперь через компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { cinemas } from "./constants/mock";
+
 export function App () {
-  return <div>Hello!</div>;
+  return <div>
+    {cinemas.map((cinema) => (
+    <div key={cinema.id}>
+      <h2>{cinema.name}</h2>
+
+      {cinema.films.length ? (
+        <div>
+          <h3>Films</h3>
+          {cinema.films.map((film) => (
+            <div key={film.id}>{film.name}</div>
+          ))}
+        </div>
+      ) : <span>No films</span>}
+
+      <div>
+        <h3>Reviews</h3>
+        {cinema.reviews.map((review) => (
+          <div key={review.id}>{review.text}</div>
+        ))}
+      </div>
+    </div>
+    ))}
+  </div>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import { cinemas } from './constants/mock';
+import { App } from './App';
 
 const rootElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(
-  <div>
-    {cinemas.map((cinema) => (
-      <div key={cinema.id}>
-        <h2>{cinema.name}</h2>
+root.render(<App />);
 
-        {cinema.films.length ? (
-          <div>
-            <h3>Films</h3>
-            {cinema.films.map((film) => (
-              <div key={film.id}>{film.name}</div>
-            ))}
-          </div>
-        ) : <span>No films</span>}
-
-        <div>
-          <h3>Reviews</h3>
-          {cinema.reviews.map((review) => (
-            <div key={review.id}>{review.text}</div>
-          ))}
-        </div>
-      </div>
-    ))}
-  </div>
-);
-
-// React.createElement("div", {
-//   children: [
-//     React.createElement("button"),
-//     React.createElement("button"),
-//     React.createElement("button"),
-//     React.createElement("button"),
-//     React.createElement("button"),
-//     React.createElement("button")
-//   ],
-//   id: "myDiv"
-// })
