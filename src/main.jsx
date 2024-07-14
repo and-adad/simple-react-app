43af612845e2187bcbd72c62b41bde1965e647ef
Andrey <Andreyadad@gmail.com>
2024-07-14 23:35:08 +0500

    Создадим ситуацию что в одном из кинотеатров вообще нет фильмов выведем что нет фильмов

--- src/main.jsx
+++ src/main.jsx
@@ -11,14 +11,14 @@ root.render(
       <div key={cinema.id}>
         <h2>{cinema.name}</h2>
 
-        {!!cinema.films.length && (
+        {cinema.films.length ? (
           <div>
             <h3>Films</h3>
             {cinema.films.map((film) => (
               <div key={film.id}>{film.name}</div>
             ))}
           </div>
-        )}
+        ) : <span>No films</span>}
 
         <div>
           <h3>Reviews</h3>
