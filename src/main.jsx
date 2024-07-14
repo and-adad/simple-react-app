31ea6f7537fa843a24ed2d2d95403fc93e4e60c1
Andrey <Andreyadad@gmail.com>
2024-07-14 22:13:20 +0500

    Отреднерим название одного из кинотеатров До сих пор мы работаем в main.js чтобы показать работу реакта

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
+import { cinemas } from './constants/mock';
 
 const rootElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <div id='myDiv'>
-    <button>ClickMe</button>
-    <button>ClickMe</button>
-    <button>ClickMe</button>
-    <button>ClickMe</button>
-    <button>ClickMe</button>
+  <div>
+    <div>
+      <h2>{cinemas[0].name}</h2>
+    </div>
+
   </div>
 );
 
