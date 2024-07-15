23191ddf8bc950e247fc7055af9f9ff49bd14d66
Andrey <Andreyadad@gmail.com>
2024-07-15 15:11:10 +0500

    Все то же самое только применяя пропсы

---
+++ src/components/cinema/component.jsx
@@ -0,0 +1,21 @@
+export const Cinema = ({ cinema }) => {
+  return (
+    <div>
+        <h2>{cinema.name}</h2>
+        {cinema.films.length ? (
+        <div>
+            <h3>Films</h3>
+            {cinema.films.map((film) => (
+            <div key={film.id}>{film.name}</div>
+            ))}
+        </div>
+        ) : <span>No films</span>}
+        <div>
+        <h3>Reviews</h3>
+        {cinema.reviews.map((review) => (
+            <div key={review.id}>{review.text}</div>
+        ))}
+        </div>
+    </div>
+  );
+};
--- src/components/cinemas/component.jsx
+++ src/components/cinemas/component.jsx
@@ -1,28 +1,12 @@
 import { cinemas } from "../../constants/mock";
+import { Cinema } from "../cinema/component";
 
 export const Cimemas = () => {
   return (
   <>
       {cinemas.map((cinema) => (
-        <div key={cinema.id}>
-          <h2>{cinema.name}</h2>
-          {cinema.films.length ? (
-            <div>
-              <h3>Films</h3>
-              {cinema.films.map((film) => (
-                <div key={film.id}>{film.name}</div>
-              ))}
-            </div>
-          ) : <span>No films</span>}
-          <div>
-            <h3>Reviews</h3>
-            {cinema.reviews.map((review) => (
-              <div key={review.id}>{review.text}</div>
-            //   https://reactdev.ru/reference/react/Fragment/#rendering-a-list-of-fragments
-            ))}
-          </div>
-        </div>
-        ))};
+        <Cinema key={cinema.id} cinema={cinema} />
+      ))}
   </>
   );
 };
