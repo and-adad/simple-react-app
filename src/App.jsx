ef39db3ddc4c65dca8b15b8a48f30840dcf45218
Andrey <Andreyadad@gmail.com>
2024-07-15 17:55:51 +0500

    Отобразил Header и Footer но выглядит это не очень

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,11 @@ import { Cimemas } from "./components/cinemas/component";
 import { cinemas } from "./constants/mock";
 
 export function App () {
-  return <Cimemas cinemas={cinemas} />;
+  return (
+  <>
+    <header>Header</header>
+    <Cimemas cinemas={cinemas} />
+    <footer>Footer</footer>
+  </>
+);
 }
