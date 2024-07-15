0b59f2c83fbc0a2ef73392152f057eb0f6337d3d
Andrey <Andreyadad@gmail.com>
2024-07-15 11:40:39 +0500

    Вынес в отдельный компонент и чтобы не плодить лишние вложенные дивы в разметке оборачиваем вместо дива в реакт фрагмент

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import { cinemas } from "./constants/mock";
+import { Cimemas } from "./components/cinemas/component";
 
 export function App () {
-  return <div>
-    {cinemas.map((cinema) => (
-    <div key={cinema.id}>
-      <h2>{cinema.name}</h2>
-
-      {cinema.films.length ? (
-        <div>
-          <h3>Films</h3>
-          {cinema.films.map((film) => (
-            <div key={film.id}>{film.name}</div>
-          ))}
-        </div>
-      ) : <span>No films</span>}
-
-      <div>
-        <h3>Reviews</h3>
-        {cinema.reviews.map((review) => (
-          <div key={review.id}>{review.text}</div>
-        ))}
-      </div>
-    </div>
-    ))}
-  </div>;
+  return (
+  <>
+    <Cimemas />
+  </>
+  );
 }
