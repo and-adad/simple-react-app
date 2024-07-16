d57b04e2b509b7b6abe3e357de0bc84f8c17da35
Andrey <Andreyadad@gmail.com>
2024-07-16 12:16:39 +0500

    Сейчас отображается только первый кинотеатр

--- src/components/cinemas/component.jsx
+++ src/components/cinemas/component.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { Cinema } from "../cinema/component";
 
-export const Cimemas = ({cinemas}) => {
+export const Cimemas = ({ cinemas }) => {
+
+    const [activeCinemaIndex, setActiveCinemaIndex] = useState(0);
+    const cinema = cinemas[activeCinemaIndex];
+  
   return (
   <>
-      {cinemas.map((cinema) => (
-        <Cinema key={cinema.id} cinema={cinema} />
-      ))}
+    <div>
+
+    </div>
+    <Cinema key={cinema.id} cinema={cinema} />
+    {/* {cinemas.map((cinema) => (
+      <Cinema key={cinema.id} cinema={cinema} />
+    ))} */}
   </>
   );
 };
