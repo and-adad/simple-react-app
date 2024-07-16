8566e527146eb1de83086e44407d3c5a6a64fcc1
Andrey <Andreyadad@gmail.com>
2024-07-16 11:57:40 +0500

    Заdisable кнопки чтобы меньше нуля и больше 6 билетова нельзя было выбрать

--- src/components/film/component.jsx
+++ src/components/film/component.jsx
@@ -2,16 +2,16 @@ import { useState } from "react";
 
 export const Film = ({ film }) => {
 
-  useState();
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>{film.name}</div>
       
       <div>
-        <button>-</button>
-        0
-        <button>+</button>
+        <button disabled={count === 0}>-</button>
+        {count}
+        <button disabled={count === 6}>+</button>
       </div>
     </>
   );
