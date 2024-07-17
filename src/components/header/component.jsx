77dc0b2d6786dd4ff8e0ce253e356556e8e05c2b
Andrey <Andreyadad@gmail.com>
2024-07-17 09:22:43 +0500

    Переместил в компонент header из layout В консоли

---
+++ src/components/header/component.jsx
@@ -0,0 +1,6 @@
+export const Header = () => {
+  const value = useUserContext();
+  return (
+    <div>component</div>
+  )
+}
--- src/components/layout/component.jsx
+++ src/components/layout/component.jsx
@@ -1,8 +1,4 @@
-import { useUserContext } from "../../contexts/user";
-
 export const Layout = ({ children }) => {
-  const value = useUserContext();
-  console.log(value);
   return (
     <>
       <header>Header</header>
